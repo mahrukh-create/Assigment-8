9474f8293de1bc44b918631d6b33d4895bbe9116
Mahrukh Ansari <mahrukh@mail.adelphi.edu>
2026-05-12 14:12:38 -0400

    Mahrukh TriangleTool.java

--- TriangleTool.java
+++ TriangleTool.java
@@ -1,15 +1,14 @@
-/**
-* Represents a triangle drawing tool that extends TurtleDesigner
-* Draws an equilateral triangle along with an inner pattern.
-*
-* @author Mahrukh Ansari
-*/
-
-public class TriangleTool extends TurtleDesigner {
+    /**
+    * Represents a triangle drawing tool that extends TurtleDesigner
+    * Draws an equilateral triangle using Turtle graphics
+    *
+    * @author Mahrukh Ansari
+    */
+    public class TriangleTool extends TurtleDesigner {
 
     /**
     * Constructs a TriangleTool object according to
-    * center position, size, and color.
+    * center position, size, and color
     *
     * @param centerX the x-coordinate of the center
     * @param centerY the y-coordinate of the center
@@ -18,59 +17,41 @@ public class TriangleTool extends TurtleDesigner {
     */
     public TriangleTool(double centerX, double centerY,
     double size, String color) {
-    
+
     super(centerX, centerY, size, color);
     }
-    
+
     /**
-    * Draws the triangle shape using Turtle graphics
-    * Also adds a smaller inner triangle pattern
+    * Draws the triangle shape using Turtle graphics.
     *
     * @param turtle the turtle used for drawing
     */
     @Override
     protected void drawShape(Turtle turtle) {
-    
+
     double sideLength = getSize();
-    
+
     double height =
     sideLength * Math.sqrt(3.0) / 2.0;
-    
-    // place turtle at starting position
+
     placeTurtle(
     turtle,
-    getCenterX() - sideLength / 2.0,
-    getCenterY() - height / 3.0,
+    getCenterX() - (sideLength / 2.0),
+    getCenterY() - (height / 3.0),
     0
     );
-    
+   
     turtle.show();
-    
 
     for (int i = 0; i < 3; i++) {
     turtle.forward(sideLength);
     turtle.left(120);
     }
-    
-    placeTurtle(
-    turtle,
-    getCenterX() - sideLength / 4.0,
-    getCenterY() - height / 6.0,
-    0
-    );
-    
-    double innerSize = sideLength / 2.0;
-    
-    // draw inner triangle
-    for (int i = 0; i < 3; i++) {
-    turtle.forward(innerSize);
-    turtle.left(120);
     }
-    
 
-    for (int i = 0; i < 18; i++) {
-    turtle.forward(i * 2);
-    turtle.left(20);
-    }
+    public static void main(String[] args) {
+        TriangleTool tri = new TriangleTool(150,150,200,"blue");
+        tri.draw();
+        
     }
     }
